ffaee4f5db7547ee137c01f67b447fafc90d80c9
Pranjal Gandhi <nicepranjal@gmail.com>
2021-08-09 12:01:29 +0530

    Files added

--- src/main/scala/com/igniteplus/data/pipeline/service/PipelineService.scala
+++ src/main/scala/com/igniteplus/data/pipeline/service/PipelineService.scala
@@ -13,27 +13,27 @@ object PipelineService {
     val dfItemData = readFile(ITEM_DATASET,FILE_FORMAT)
     dfItemData.printSchema()
 
-    val df = readFile("data/input02/clickstream/empty1.csv",FILE_FORMAT)
+//    val df = readFile("data/input02/clickstream/empty1.csv",FILE_FORMAT)
 
-//    val dfViewLog = readFile(CLICKSTREAM_DATASET,FILE_FORMAT)
-//    dfViewLog.printSchema()
-//
-//    val dfViewLogDtaTypeValidated = dataTypeValidation(dfViewLog,COL_NAME_DATATYPE_DF1,DATATYPE_DF1)
-//    dfViewLogDtaTypeValidated .printSchema()
-//
-//    val dfViewLogTrimmed = trimColumn(dfViewLogDtaTypeValidated ,COLS_OF_CLICKSTREAM)
-//    val dfItemDataTrimmed = trimColumn(dfItemData,COLS_OF_ITEMDATA)
-//
-//    FilterNullRow(dfViewLogTrimmed,NULLKEY_CLICKSTREAM,CLICKSTREAM_DATASET_NULL)
-//    FilterNullRow(dfItemDataTrimmed,NULLKEY_ITEM,ITEM_DATASET_NULL)
-//
-//    val dfViewLogNotNull = separateNotNull(dfViewLogTrimmed,NULLKEY_CLICKSTREAM)
-//    val dfItemDataNotNull = separateNotNull(dfItemDataTrimmed,NULLKEY_ITEM)
-//
-//    val dfViewRemovedDuplicates = removeDuplicates(dfViewLogNotNull,NULLKEY_CLICKSTREAM,EVENT_TIMESTAMP)
-//    //    println(dfViewRemovedDuplicates.count())
-//    val dfItemRemovedDuplicates = removeDuplicates(dfItemDataNotNull,NULLKEY_ITEM)
-//    //    println(dfItemRemovedDuplicates.count())
+    val dfViewLog = readFile(CLICKSTREAM_DATASET,FILE_FORMAT)
+    dfViewLog.printSchema()
+
+    val dfViewLogDtaTypeValidated = dataTypeValidation(dfViewLog,COL_NAME_DATATYPE_DF1,DATATYPE_DF1)
+    dfViewLogDtaTypeValidated .printSchema()
+
+    val dfViewLogTrimmed = trimColumn(dfViewLogDtaTypeValidated ,COLS_OF_CLICKSTREAM)
+    val dfItemDataTrimmed = trimColumn(dfItemData,COLS_OF_ITEMDATA)
+
+    FilterNullRow(dfViewLogTrimmed,NULLKEY_CLICKSTREAM,CLICKSTREAM_DATASET_NULL)
+    FilterNullRow(dfItemDataTrimmed,NULLKEY_ITEM,ITEM_DATASET_NULL)
+
+    val dfViewLogNotNull = separateNotNull(dfViewLogTrimmed,NULLKEY_CLICKSTREAM)
+    val dfItemDataNotNull = separateNotNull(dfItemDataTrimmed,NULLKEY_ITEM)
+
+    val dfViewRemovedDuplicates = removeDuplicates(dfViewLogNotNull,NULLKEY_CLICKSTREAM,EVENT_TIMESTAMP)
+    //    println(dfViewRemovedDuplicates.count())
+    val dfItemRemovedDuplicates = removeDuplicates(dfItemDataNotNull,NULLKEY_ITEM)
+    //    println(dfItemRemovedDuplicates.count())
 
     //    dfViewLog.printSchema()
     //    writeFile(dfViewLog.filter(dfViewLog("device_type") === "web"),"data/input02/item/item.csv","csv"
